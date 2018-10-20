b6e8f1f0b94a9277fb219f9151fc97bd579f39af
Diego Varela <diegov09f@hotmail.es>
2018-10-20 17:49:08 -0600

    check

--- src/main/java/com/toursadvisorcr/controller/IndexController.java
+++ src/main/java/com/toursadvisorcr/controller/IndexController.java
@@ -1,10 +1,9 @@
 package com.toursadvisorcr.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /** The type Index controller. */
-@Controller
+//@Controller
 public class IndexController {
   /**
    * Index string.
